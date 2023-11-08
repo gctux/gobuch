77e3a77e38902057746ee5547486fa6616cb8971
Lutz Herrmann <lutz.herrmann@mailbox.org>
2023-11-08 09:42:57 +0100

    first commit

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	var nr = 2
 	var name = "Lutz"
-	fmt.Printf("%03d: Hallo, %s\n", nr, name)
+	s := fmt.Sprintf("%03d: Hallo, %s\n", nr, name)
+	fmt.Print(s)
 }
 
 // Output:
