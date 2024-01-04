c3a7151c010d7978eb841ac041f074859acc4556
Lutz Herrmann <lutz.herrmann@mailbox.org>
2024-01-04 13:01:56 +0100

    first commit

--- gomd5/main.go
+++ gomd5/main.go
@@ -2,24 +2,56 @@ package main
 
 import (
 	"crypto/md5"
+	"flag"
 	"fmt"
 	"io"
-	"log"
+	"net/http"
 	"os"
 )
 
-func main() {
+var (
+	flagFile = flag.String(
+		"file",
+		"",
+		"File als direkter Input")
+	flagURL = flag.String(
+		"url",
+		"",
+		"URL als direkter Input")
+)
 
-	h := md5.New()
-	if _, err := io.Copy(h, os.Stdin); err != nil {
-		log.Fatal(err)
+func main() {
+	flag.Parse()
+	var input io.Reader = os.Stdin
+	var output io.Writer = os.Stdout
+	switch {
+	case *flagFile != "":
+		fd, err := os.Open(*flagFile)
+		if err != nil {
+			fmt.Fprintln(output, "Fehler beim Öffnen des Files: ", err)
+			os.Exit(1)
+		}
+		defer fd.Close()
+		input = fd
+	case *flagURL != "":
+		resp, err := http.Get(*flagURL)
+		if err != nil {
+			fmt.Fprintln(output, "Fehler beim Abrufen der URL: ", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		input = resp.Body
 	}
 
-	_, err := io.Copy(h, os.Stdin)
+	printMD5(input, os.Stdout)
+}
+
+func printMD5(r io.Reader, w io.Writer) {
+	h := md5.New()
+	_, err := io.Copy(h, r)
 	if err != nil {
 		fmt.Println("Fehler beim Einlesen:", err)
-		os.Exit(1)
+		return
 	}
-
-	fmt.Printf("%x\n", h.Sum(nil))
+	fmt.Fprintf(w, "%x\n", h.Sum(nil))
 }
